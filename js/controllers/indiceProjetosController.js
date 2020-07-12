e0537f0327c9b7ad1374600d2a72364d073c806d
Cleber Presidente 100% <cleberpresidenteanhanguera@gmail.com>
2020-07-12 16:40:56 -0300

    Criação de um Service.

--- js/controllers/indiceProjetosController.js
+++ js/controllers/indiceProjetosController.js
@@ -1,162 +1,13 @@
 
 angular.module("indiceProjetos", []);
-angular.module("indiceProjetos").controller("indiceProjetosController", function($scope, $location){
+angular.module("indiceProjetos").controller("indiceProjetosController", function($scope, arvoreMenusAPI){
     
     $scope.numero = 0;
     $scope.nomeDaPagina = "Índice de Projetos"
     $scope.arvoreMenusDireita = [];
     $scope.arvoreMenusEsquerda = [];
+    $scope.arvoreMenus = arvoreMenusAPI.getArvoreMenus();
 
-    // Estrutura que contém as informações que serão montadas na view.
-    $scope.estruturaBase = [
-        {
-            id : 0,
-            estado : false, // TRUE --> Árvore Aberta, FALSE --> Árvore Fechada
-            titulo : "Inteligência Artificial",
-            subTitulos : [
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Python Fundamentos",
-                    projetos : [
-                        {nome : "Teste 01", endereco : "#"},
-                        {nome : "Teste 02", endereco : "#"},
-                        {nome : "Teste 03", endereco : "#"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Deep Learning Book",
-                    projetos : [
-                        {nome : "Teste 01", endereco : "#"},
-                        {nome : "Teste 02", endereco : "#"},
-                        {nome : "Teste 03", endereco : "#"},
-                    ],
-                },
-            ],
-        },
-        {
-            id : 0,
-            estado : false,
-            titulo : "Anhanguera",
-            subTitulos : [
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Compiladores",
-                    projetos : [
-                        {nome : "Tabela de Símbolos", endereco : "https://github.com/CleberPresidente100/TabelaDeSimbolos"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Linguagens Formais e Autômatos",
-                    projetos : [
-                        {nome : "Expressões Matemáticas", endereco : "https://github.com/CleberPresidente100/Expressoes_Matematicas"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Algoritmos e Estrutura de Dados",
-                    projetos : [
-                        {nome : "Jogo \"É Duro\"", endereco : "https://github.com/CleberPresidente100/Jogo_E_Duro"},
-                        {nome : "Projeto de Final de Semestre", endereco : "https://github.com/CleberPresidente100/Projeto_Final_de_Semestre"},
-                    ],
-                },
-            ],                        
-        },
-        {
-            id : 0,
-            estado : false,
-            titulo : "Udemy",
-            subTitulos : [
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Flutter",
-                    projetos : [
-                        {nome : "Todos os Exercícios", endereco : "https://github.com/CleberPresidente100/Udemy_Flutter"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Teste X",
-                    projetos : [
-                        {nome : "Teste 01", endereco : "#"},
-                        {nome : "Teste 02", endereco : "#"},
-                        {nome : "Teste 03", endereco : "#"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Teste Y",
-                    projetos : [
-                        {nome : "Teste 01", endereco : "#"},
-                        {nome : "Teste 02", endereco : "#"},
-                        {nome : "Teste 03", endereco : "#"},
-                    ],
-                },
-            ],                        
-        },
-        {
-            id : 0,
-            estado : false,
-            titulo : "Outros",
-            subTitulos : [
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Python",
-                    projetos : [
-                        {nome : "Força Bruta p/ arquivos RAR", endereco : "https://github.com/CleberPresidente100/Python_RAR_Brute_Force"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "PHP",
-                    projetos : [
-                        {nome : "Planetinha", endereco : "#"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "AngularJS",
-                    projetos : [
-                        {nome : "Todos os Exercícios", endereco : "https://github.com/CleberPresidente100/AngularJS"},
-                    ],
-                },
-                {
-                    id : 0,
-                    estado : false,
-                    nome : "Websites",
-                    projetos : [
-                        {nome : "Planetinha", endereco : "#"},
-                        {nome : "Índice de Projetos - (Esta Página)", endereco : "https://github.com/CleberPresidente100/Indice_de_Projetos"},
-                    ],
-                },
-            ],                        
-        },
-    ];
-
-    // Em um banco os IDs seriam as chaves primárias numéricas.
-    // Como esta consulta não está sendo feita em um banco, os IDs precisam ser gerados de alguma forma.
-    // Esta função automatiza esta geração de IDs
-    $scope.gerarIds = function(estrutura){
-        var contador = 1;
-        estrutura.forEach(elemento => {
-            elemento.id = contador++;
-            elemento.subTitulos.forEach(subTitulo => {
-                subTitulo.id = contador++;
-            });
-        });
-    };
 
     
     $scope.alternarVisibilidade = function(estrutura, tituloId, subTituloId){                    
@@ -189,7 +40,7 @@ angular.module("indiceProjetos").controller("indiceProjetosController", function
 
 
     // Divide a Lista de Menus em duas colunas
-    $scope.splitEstruturaBase = function(estrutura){
+    $scope.splitArvoreMenusBase = function(estrutura){
         
         var tamanho = estrutura.length;
         var metade = tamanho / 2;
@@ -208,7 +59,6 @@ angular.module("indiceProjetos").controller("indiceProjetosController", function
 
 
     // Execução das funções
-    $scope.gerarIds($scope.estruturaBase);
-    $scope.splitEstruturaBase($scope.estruturaBase);
+    $scope.splitArvoreMenusBase($scope.arvoreMenus);
     
 });
---
+++ js/services/arvoreMenusService.js
@@ -0,0 +1,148 @@
+
+angular.module("indiceProjetos").factory("arvoreMenusAPI", function (){
+
+    // Estrutura que contém as informações do Menu que serão Montadas no Controller e Exibidas na View.
+    var arvoreMenus = [
+        {
+            id : 0,
+            estado : false, // TRUE --> Árvore Aberta, FALSE --> Árvore Fechada
+            titulo : "Inteligência Artificial",
+            subTitulos : [
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Python Fundamentos",
+                    projetos : [
+                        {nome : "Vazio", endereco : "#"},
+                    ],
+                },
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Deep Learning Book",
+                    projetos : [
+                        {nome : "Vazio", endereco : "#"},
+                    ],
+                },
+            ],
+        },
+        {
+            id : 0,
+            estado : false,
+            titulo : "Anhanguera",
+            subTitulos : [
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Compiladores",
+                    projetos : [
+                        {nome : "Tabela de Símbolos", endereco : "https://github.com/CleberPresidente100/TabelaDeSimbolos"},
+                    ],
+                },
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Linguagens Formais e Autômatos",
+                    projetos : [
+                        {nome : "Expressões Matemáticas", endereco : "https://github.com/CleberPresidente100/Expressoes_Matematicas"},
+                    ],
+                },
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Algoritmos e Estrutura de Dados",
+                    projetos : [
+                        {nome : "Jogo \"É Duro\"", endereco : "https://github.com/CleberPresidente100/Jogo_E_Duro"},
+                        {nome : "Projeto de Final de Semestre", endereco : "https://github.com/CleberPresidente100/Projeto_Final_de_Semestre"},
+                    ],
+                },
+            ],                        
+        },
+        {
+            id : 0,
+            estado : false,
+            titulo : "Udemy",
+            subTitulos : [
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Flutter",
+                    projetos : [
+                        {nome : "Todos os Exercícios", endereco : "https://github.com/CleberPresidente100/Udemy_Flutter"},
+                    ],
+                },
+            ],                        
+        },
+        {
+            id : 0,
+            estado : false,
+            titulo : "Outros",
+            subTitulos : [
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Python",
+                    projetos : [
+                        {nome : "Força Bruta p/ arquivos RAR", endereco : "https://github.com/CleberPresidente100/Python_RAR_Brute_Force"},
+                    ],
+                },
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "PHP",
+                    projetos : [
+                        {nome : "Planetinha", endereco : "#"},
+                    ],
+                },
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "AngularJS",
+                    projetos : [
+                        {nome : "Todos os Exercícios", endereco : "https://github.com/CleberPresidente100/AngularJS"},
+                    ],
+                },
+                {
+                    id : 0,
+                    estado : false,
+                    nome : "Websites",
+                    projetos : [
+                        {nome : "Planetinha", endereco : "#"},
+                        {nome : "Índice de Projetos - (Esta Página)", endereco : "https://github.com/CleberPresidente100/Indice_de_Projetos"},
+                    ],
+                },
+            ],                        
+        },
+    ];
+
+
+
+    // Em um banco os IDs seriam as chaves primárias numéricas.
+    // Como esta consulta não está sendo feita em um banco, os IDs precisam ser gerados de alguma forma.
+    // Esta função automatiza esta geração de IDs para que o Angular possa identificar e alterar as propriedades de cada item do menu de forma dinâmica.
+    var _gerarIds = function(estrutura){
+        if(estrutura){
+            var contador = 1;
+            estrutura.forEach(elemento => {
+                elemento.id = contador++;
+                elemento.subTitulos.forEach(subTitulo => {
+                    subTitulo.id = contador++;
+                });
+            });
+        }
+    };
+
+    
+
+    var _getArvoreMenus = function () {
+        _gerarIds(arvoreMenus);
+        return arvoreMenus;
+    };
+
+
+
+    return {
+        getArvoreMenus: _getArvoreMenus,
+    };
+
+})
